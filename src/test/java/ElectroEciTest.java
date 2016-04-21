f9d985d92e2ffa6cdf63a8a2841aeba1bf652645
Santiago <david.chisco@mail.escuelaing.edu.co>
2016-04-21 15:56:13 -0500

    Heruko y Circle

---
+++ src/test/java/ElectroEciTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 2100609
+ */
+public class ElectroEciTest {
+    
+    public ElectroEciTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+        
+    
+     @Test
+     public void PruebaDeInicio() {
+         assertTrue(true);
+         
+     }
+}
